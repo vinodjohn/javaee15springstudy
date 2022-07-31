dc2c6d0009580dfaffcbf430db8c88541c6d9973
Vinod John <vinod.infant.dass.john.rozario@luminorgroup.com>
2022-07-31 13:36:40 +0300

    Fragments implementation

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $(".alert-success, .alert-danger").fadeTo(5000, 1000).slideUp(1000, function () {
         $(".alert-success, .alert-danger").slideUp(1000);
     });
+
+    $(document).on('click', '.nav-item a', function(e) {
+       $(this).addClass('active').siblings().removeClass('active');
+    });
 });
